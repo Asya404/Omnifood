ddc62b7fbd6f50178a78544a8245ea8c78c2a4e4
Asya404 <unkiplum@gmail.com>
2021-03-05 20:51:10 +0100

    added animations on scroll

--- js/script.js
+++ js/script.js
@@ -1,5 +1,6 @@
 $(document).ready(function () {
 
+   /* Waipoints for triggering functions on scroll */
    /* For the sticky navigation */
    $('.js--section-features').waypoint(function (direction) {
       if (direction == "down") {
@@ -37,6 +38,35 @@ $(document).ready(function () {
          }
       });
    });
+
+
+   /* Animations on scroll */
+   $('.js--wp-1').waypoint(function (direction) {
+      $('.js--wp-1').addClass('animate__animated animate__fadeIn')
+   }, {
+      offset: '50 %'
+   })
+
+
+   $('.js--wp-2').waypoint(function (direction) {
+      $('.js--wp-2').addClass('animate__animated animate__fadeInUp')
+   }, {
+      offset: '50 %'
+   })
+
+
+   $('.js--wp-3').waypoint(function (direction) {
+      $('.js--wp-3').addClass('animate__animated animate__fadeIn')
+   }, {
+      offset: '50 %'
+   })
+
+
+   $('.js--wp-4').waypoint(function (direction) {
+      $('.js--wp-4').addClass('animate__animated animate__pulse')
+   }, {
+      offset: '50 %'
+   })
 
 
 
