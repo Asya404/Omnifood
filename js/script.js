0e066f6f449b737d6f9c9f73f3c2697f441e3815
Asya404 <unkiplum@gmail.com>
2021-03-07 21:51:29 +0100

    images optimization

--- js/script.js
+++ js/script.js
@@ -15,11 +15,11 @@ $(document).ready(function () {
 
    /* Scroll on buttons */
    $('.js--scroll-to-plans').click(function () {
-      $('html, body').animate({ scrollTop: $('.js--section-plans').offset().top }, 1000)
+      $('html, body').animate({ scrollTop: $('.js--section-plans').offset().top }, 100)
    })
 
    $('.js--scroll-to-start').click(function () {
-      $('html, body').animate({ scrollTop: $('.js--section-features').offset().top }, 1000)
+      $('html, body').animate({ scrollTop: $('.js--section-features').offset().top }, 100)
    })
 
 
